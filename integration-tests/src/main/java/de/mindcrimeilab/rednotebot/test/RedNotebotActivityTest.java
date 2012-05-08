13c5badae2f61b3e61a7d919e72198e3fb386312
Michael Engelhardt <me@mindcrime-ilab.de>
2012-05-08 22:42:19 +0200

    integration tests needs more work to work...

--- integration-tests/src/main/java/de/mindcrimeilab/rednotebot/test/RedNotebotActivityTest.java
+++ integration-tests/src/main/java/de/mindcrimeilab/rednotebot/test/RedNotebotActivityTest.java
@@ -1,11 +1,13 @@
 package de.mindcrimeilab.rednotebot.test;
 
+import android.support.v4.app.Fragment;
 import android.test.ActivityInstrumentationTestCase2;
+import de.mindcrimeilab.rednotebot.R;
 import de.mindcrimeilab.rednotebot.RedNotebotActivity;
 
 public class RedNotebotActivityTest extends ActivityInstrumentationTestCase2<RedNotebotActivity> {
 
-    private RedNotebotActivity tActivity;  // the activity under test
+    private RedNotebotActivity testee;  // the activity under test
 
     public RedNotebotActivityTest() {
         super("de.mindcrimeilab.rednotebot",RedNotebotActivity.class);
@@ -14,10 +16,13 @@ public class RedNotebotActivityTest extends ActivityInstrumentationTestCase2<Red
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        tActivity = this.getActivity();
+        
+        testee = this.getActivity();
     }
 
     public void testPreconditions() {
-        assertNotNull(tActivity);
+        assertNotNull(testee);
+        Fragment dayListFragment = testee.getSupportFragmentManager().findFragmentById(R.id.daylist);
+        assertNotNull(dayListFragment);
     }
 }
