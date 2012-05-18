01e3350112c1602aa15fc3f7d3a261975c985a3a
Michael Engelhardt <me@mindcrime-ilab.de>
2012-05-18 22:27:18 +0200

    implementing content provider

---
+++ app/src/main/java/de/mindcrimeilab/rednotebot/providers/dropbox/ContentAccessMethods.java
@@ -0,0 +1,49 @@
+/*
+ * RedNotebot - Mobile dairy, journal, notekeeping tool. Copyright (C) 2012 Michael Engelhardt
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package de.mindcrimeilab.rednotebot.providers.dropbox;
+
+/**
+ * Enumeration describing the various content access method e.g. Dropbox or local file storage.
+ * 
+ * @author Michael Engelhardt <me@mindcrime-ilab.de>
+ * 
+ */
+public enum ContentAccessMethods {
+    LOCAL_STORAGE, DROPBOX;
+
+    /**
+     * Converts a {@code String} to a {@code ContentAccessMethods} identifier. This method will default to {@value
+     * ContentAccessMethods.LOCAL_STORAGE} if the parsing of the string will fail.
+     * 
+     * @param value
+     * @return
+     */
+    static ContentAccessMethods from(final String value) {
+        final ContentAccessMethods ret;
+        if ("0".equals(value)) ret = LOCAL_STORAGE;
+        else if ("1".equals(value)) ret = DROPBOX;
+        else ret = LOCAL_STORAGE;
+        return ret;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return String.format("%s[%s]", ContentAccessMethods.class, name());
+    }
+}
--- app/src/main/java/de/mindcrimeilab/rednotebot/providers/dropbox/RedNotebotContentProvider.java
+++ app/src/main/java/de/mindcrimeilab/rednotebot/providers/dropbox/RedNotebotContentProvider.java
@@ -13,13 +13,24 @@
  */
 package de.mindcrimeilab.rednotebot.providers.dropbox;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import de.akquinet.android.androlog.Log;
+import de.mindcrimeilab.rednotebot.LogTags;
+import de.mindcrimeilab.rednotebot.PreferenceKeys;
+import de.mindcrimeilab.rednotebot.data.MonthEntry;
 
 /**
  * Content provider to access various sources of journal entries.
@@ -31,13 +42,16 @@ public class RedNotebotContentProvider extends ContentProvider implements OnShar
 
     /** URI matcher */
     private final static UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
-    
+
     /** constant for month uris */
     private final static int MONTH = 1;
 
     /** constant for day uris */
     private final static int DAY = 2;
-    
+
+    /** content cache */
+    private Map<String, CacheEntry> cache = new WeakHashMap<String, CacheEntry>(31);
+
     /**
      * static initialization of URI matcher
      */
@@ -45,7 +59,7 @@ public class RedNotebotContentProvider extends ContentProvider implements OnShar
         URI_MATCHER.addURI("de.mindcrimeilab.rednotebot", "month", MONTH);
         URI_MATCHER.addURI("de.mindcrimeilab.rednotebot", "day", DAY);
     }
-    
+
     /*
      * (non-Javadoc)
      * 
@@ -54,8 +68,27 @@ public class RedNotebotContentProvider extends ContentProvider implements OnShar
      * SharedPreferences, java.lang.String)
      */
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        // TODO Auto-generated method stub
+        // reflect changes to the content access method in the content provider; e.g. get notified about switching from
+        // local file storage to dropbox remote storage.
+        Log.v(LogTags.CONTENT_PROVIDER, String.format("Notification of preference change for key [%s]; preferences=[%s]", key, sharedPreferences));
 
+        PreferenceKeys prefKey = PreferenceKeys.valueOf(key);
+        Log.v(LogTags.CONTENT_PROVIDER, String.format("Converted key [%s] is [%s]", key, prefKey));
+
+        switch (prefKey) {
+            case DATAPROVIDER: {
+                // set content access method
+                break;
+            }
+            case DROPBOX_KEY: {
+                // set dropbox session key
+                break;
+            }
+            case DROPBOX_SECRET: {
+                // set dropbix session secret
+                break;
+            }
+        }
     }
 
     /*
@@ -98,8 +131,35 @@ public class RedNotebotContentProvider extends ContentProvider implements OnShar
      */
     @Override
     public boolean onCreate() {
-        // TODO Auto-generated method stub
-        return false;
+        // setup preference listener
+        Context context = getContext();
+        Log.v(LogTags.CONTENT_PROVIDER, String.format("Obtained context is [%s]", context));
+        Context applicationContext = context.getApplicationContext();
+        Log.v(LogTags.CONTENT_PROVIDER, String.format("Obtained application context is [%s]", applicationContext));
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+        prefs.registerOnSharedPreferenceChangeListener(this);
+
+        // init content provider from shared preferences
+        onSharedPreferenceChanged(prefs, PreferenceKeys.DATAPROVIDER.toString());
+        onSharedPreferenceChanged(prefs, PreferenceKeys.DROPBOX_KEY.toString());
+        onSharedPreferenceChanged(prefs, PreferenceKeys.DROPBOX_SECRET.toString());
+
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see android.content.ContentProvider#onLowMemory()
+     */
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        // clean up memory
+        // @TODO: implement an advanced clean up mechanism (if necessary) later. For now {@code WeakHashMap} should do
+        // the job
+        this.cache.clear();
+
     }
 
     /*
@@ -110,8 +170,56 @@ public class RedNotebotContentProvider extends ContentProvider implements OnShar
      */
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        // TODO Auto-generated method stub
-        return null;
+        Log.d(LogTags.CONTENT_PROVIDER, String.format("Loading [%s]...", uri.toString()));
+
+        // normalize / 'beautify' path
+        String path = uri.getPath().replaceAll("//", "/");
+
+        // first try to get a cached version; prefix the uri with the content access method to prevent cache collisions
+        // when using various content access methods.
+        String cacheKey = String.format("%s%s", getContentAccessMethod().name(), path);
+
+        Log.v(LogTags.CONTENT_PROVIDER, String.format("Try to load content for cache key [%s]", cacheKey));
+        CacheEntry cachedEntry = cache.get(cacheKey);
+
+        if (null == cachedEntry) {
+            Log.v(LogTags.CONTENT_PROVIDER, "No cache hit.");
+        }
+        else {
+            Log.v(LogTags.CONTENT_PROVIDER, String.format("Principle cache hit [%s] having modifcation time [%d].", cachedEntry.getEntry(), cachedEntry.getTimestamp()));
+        }
+
+        // (re-)load if there where either no matching entry or the entry is invalidated by a newer version
+        final MonthEntry entry;
+        if (null == cachedEntry || isNewer(path, cachedEntry)) {
+            entry = load(path);
+            cache.put(cacheKey, new CacheEntry(entry));
+            Log.d(LogTags.CONTENT_PROVIDER, String.format("Add new cache key [%s].", cacheKey));
+        }
+        else {
+            Log.d(LogTags.CONTENT_PROVIDER, String.format("Cache hit, using content from key [%s].", cacheKey));
+            entry = cachedEntry.getEntry();
+        }
+
+        final Cursor cursor;
+        if (null == entry) {
+            cursor = new MatrixCursor(new String[] { "_ID"});
+        }
+        else {
+            // decide whether it requests the whole month entry or only a daily one. A day entry is identified by a
+            // fragment identifier eg. #21 fo the 21st day in one moth
+            String fragment = uri.getFragment();
+            if (TextUtils.isEmpty(fragment)) {
+                cursor = parseMonth(entry);
+            }
+            else {
+                cursor = parseDay(entry, fragment);
+            }
+        }
+
+        Log.d(LogTags.CONTENT_PROVIDER, String.format("Loading of [%s] finished.", uri.toString()));
+
+        return cursor;
     }
 
     /*
@@ -124,6 +232,166 @@ public class RedNotebotContentProvider extends ContentProvider implements OnShar
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    /* ==================================================================== */
+    /*
+     * PUBLIC API
+     */
+    /* ==================================================================== */
+
+    /* ==================================================================== */
+    /*
+     * PRIVATE API
+     */
+    /* ==================================================================== */
+    /**
+     * Get current method of content access
+     * 
+     * @return
+     */
+    private ContentAccessMethods getContentAccessMethod() {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    /**
+     * Determine if the cached entry is valid.
+     */
+    private boolean isNewer(final String path, final CacheEntry entry) {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    /**
+     * loads a entry from underlying implementation
+     */
+    private MonthEntry load(final String path) {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    /**
+     * Returns a cursor representing all information about a certain day entry
+     * 
+     * @param entry
+     * @param fragment
+     * 
+     */
+    private Cursor parseDay(final MonthEntry entry, final String fragment) {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    /**
+     * Returns a cursor representing all information about a month entry
+     * 
+     * @param entry
+     */
+    private Cursor parseMonth(final MonthEntry entry) {
+        throw new RuntimeException("Not implemented!");
+    }
+
+    /* ==================================================================== */
+    /*
+     * INNER CLASSES
+     */
+    /* ==================================================================== */
+    /**
+     * Internal class representing an entry in the cache. The cache is used to facilitate the access to content more
+     * efficient, in particular if it is necessary to load content from remote sites.
+     * 
+     * @author Michael Engelhardt <me@mindcrime-ilab.de>
+     * 
+     */
+    /**
+     * @author Michael Engelhardt <me@mindcrime-ilab.de>
+     * 
+     */
+    final static class CacheEntry {
+
+        /** cached content */
+        private final MonthEntry entry;
+
+        /** timestamp - base for TTL calculation */
+        private final long timestamp;
+
+        /**
+         * ctor() Instantiate a new cache entry initialized with the current timestamp
+         */
+        public CacheEntry(final MonthEntry entry) {
+            this.timestamp = System.nanoTime();
+            this.entry = entry;
+        }
+
+        /**
+         * Returns timestamp of certain cache entry.
+         * 
+         * @return
+         */
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        /**
+         * Return cached instance of {@code MonthEntry}
+         * 
+         * @return
+         */
+        MonthEntry getEntry() {
+            return entry;
+        }
+
+        /**
+         * Returns true if the given timestamp is newer than the cached one.
+         */
+        boolean isNewer(long timestamp) {
+            return timestamp > this.timestamp;
+        }
+
+        /*
+         * Object infrastructure
+         */
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Object#hashCode()
+         */
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((entry == null) ? 0 : entry.hashCode());
+            result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+            return result;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            CacheEntry other = (CacheEntry) obj;
+            if (entry == null) {
+                if (other.entry != null) return false;
+            }
+            else if (!entry.equals(other.entry)) return false;
+            if (timestamp != other.timestamp) return false;
+            return true;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Object#toString()
+         */
+        @Override
+        public String toString() {
+            return "CacheEntry [entry=" + entry + ", timestamp=" + timestamp + "]";
+        }
+
     }
 
 }
